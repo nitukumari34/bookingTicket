690d7d47b2f71d028ebe932dc8686fa3dc7db89b
Nitu Kumari <nituspj032001@gmail.com>
2026-09-12 13:28:50 +0530

    feat: add seat controller endpoints and auto-generate show seats during creation

--- src/main/java/com/bookingTicket/controller/SeatController.java
+++ src/main/java/com/bookingTicket/controller/SeatController.java
@@ -1,4 +1,29 @@
 package com.bookingTicket.controller;
 
+import com.bookingTicket.entities.Seat;
+import com.bookingTicket.service.SeatService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/seats")
 public class SeatController {
+
+    private final SeatService seatService;
+
+    public SeatController(SeatService seatService) {
+        this.seatService = seatService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Seat> addSeat(@RequestBody Seat seat) {
+        return ResponseEntity.ok(seatService.addSeat(seat));
+    }
+
+    @GetMapping("/hall/{hallId}")
+    public ResponseEntity<List<Seat>> getSeatsByHall(@PathVariable Long hallId) {
+        return ResponseEntity.ok(seatService.getSeatsByHall(hallId));
+    }
 }
