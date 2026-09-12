46daf0e90607e09d71b99da4eec9a3931d7696ec
Nitu Kumari <nituspj032001@gmail.com>
2026-09-12 13:58:32 +0530

    feat: add payment strategies, event-driven notifications, and booking management logic

--- src/main/java/com/bookingTicket/entities/Payment.java
+++ src/main/java/com/bookingTicket/entities/Payment.java
@@ -1,8 +1,29 @@
 package com.bookingTicket.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
-    private  Long id;
-    private  Long bookingId;
-    private  double amount;
-    private  PaymentStatus status;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long bookingId;
+
+    private double amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    private String transactionId;
 }
